4caf0ae54fb2ca2672c5fcd0076ce5a7f48cabc6
Anup Vasudevan <anupv@Anups-MacBook-Air.local>
2021-10-14 20:36:53 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var randomNumber = 1 + Math.floor(Math.random() * 100);
+var guess =  document.getElementById('userGuess');
+
+document.getElementById('checkGuess').addEventListener('click', function(event) {
+    // write some code here
+});
